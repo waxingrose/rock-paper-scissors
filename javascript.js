813c20c26f69433dbd4f7d741a4f0315dbef7814
Lenna <128262375+waxingrose@users.noreply.github.com>
2023-09-06 21:45:50 -0400

    fixed computer generator

--- javascript.js
+++ javascript.js
@@ -1,53 +1,61 @@
-let getComputerChoice = Math.floor(Math.random() * 3); /* randomizer 0, 1, 2 */
-console.log(getComputerChoice);
-switch (getComputerChoice) {
-  case 0:
-        Return = 'Rock';
-    break;
-  case 1:
-        Return = 'Paper';
-    break;
-  case 2:
-        Return = 'Scissors';
-    break;
-}
+let computerSelection = function getComputerChoice () { 
+    switch (Math.floor(Math.random() * 3)) {  /* randomizer 0, 1, 2 */
+        case 0:
+            Return = 'Rock';
+            break;
+        case 1:
+            Return = 'Paper';
+            break;
+        case 2:
+            Return = 'Scissors';
+            break;
+    }
+    Return;
+};
 console.log(Return);
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+    if (playerSelection == 'Paper' && Return == 'Rock') {
         alert('You win! Paper beats Rock!');
         console.log('You win! Paper beats Rock!');
-        ++score;
+        console.log('You:' + ++playerScore);
+        console.log('Computer:' + computerScore);
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    else if (playerSelection == 'Scissors' && Return == 'Paper') {
         alert('You win! Scissors beats Paper!');
         console.log('You win! Scissors beats Paper!');
-        ++score;
+        console.log('You:' + ++playerScore);
+        console.log('Computer:' + computerScore);
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    else if (playerSelection == 'Rock' && Return == 'Scissors') {
         alert('You win! Rock beats Scissors');
         console.log('You win! Rock beats Scissors!');
-        ++score;
+        console.log('You:' + ++playerScore);
+        console.log('Computer:' + computerScore);
     }
-    else if (playerSelection == computerSelection) {
+    else if (playerSelection == Return) {
         alert('Tie!');
         console.log('Tie!');
-        score;
+        console.log('You:' + playerScore);
+        console.log('Computer:' + computerScore);
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+    else if (playerSelection == 'Scissors' && Return == 'Rock') {
         alert('You lose! Rock beats Scissors.');
         console.log('You lose!');
-        score;
+        console.log('You:' + playerScore);
+        console.log('Computer:' + ++computerScore);
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+    else if (playerSelection == 'Rock' && Return == 'Paper') {
         alert('You lose! Paper beats Rock.');
         console.log('You lose!');
-        score;
+        console.log('You:' + playerScore);
+        console.log('Computer:' + ++computerScore);
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+    else if (playerSelection == 'Paper' && Return == 'Scissors') {
         alert('You lose! Scissors beats Paper.');
         console.log('You lose!');
-        score;
+        console.log('You:' + playerScore);
+        console.log('Computer:' + ++computerScore);
     }
     else {
         null
@@ -58,15 +66,29 @@ function playRound(playerSelection, computerSelection) {
 
 /* Assigned variables */
 let playerSelection
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function Game() {
-    if (score > 3) {
-        console.log('You win!')
+    if (playerScore > computerScore) {
+        alert('Congratulations! You beat the computer!')
+        console.log('Congratulations! You beat the computer!')
+    }
+    else if (playerScore < computerScore) {
+        alert('You lost against the computer!')
+        console.log('You lost against the computer!')
+    }   
+    else if (playerScore == computerScore) {
+        alert('Game is a tie!')
+        console.log('Game is a tie!')
+    }
+    else {
+        null;
     }
 }
 
-Game(playRound(prompt('Rock, Paper, or Scissors?'), Return)); 
-Game(playRound(prompt('Rock, Paper, or Scissors?'), Return)); 
-Game(playRound(prompt('Rock, Paper, or Scissors?'), Return)); 
-Game(playRound(prompt('Rock, Paper, or Scissors?'), Return)); 
+playRound(prompt('Rock, Paper, or Scissors?'), computerSelection()); 
+playRound(prompt('Rock, Paper, or Scissors?'), computerSelection()); 
+playRound(prompt('Rock, Paper, or Scissors?'), computerSelection());  
+playRound(prompt('Rock, Paper, or Scissors?'), computerSelection());  
+Game(playRound(prompt('Rock, Paper, or Scissors?'), computerSelection())); 
