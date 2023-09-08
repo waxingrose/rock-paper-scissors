e5f3d319069d7de14ff25357cf0a5844b7fe86db
Lenna <128262375+waxingrose@users.noreply.github.com>
2023-09-08 17:08:54 -0400

    added scoring feature to game() function

--- javascript.js
+++ javascript.js
@@ -18,44 +18,37 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
         alert('You win! Paper beats Rock!');
         console.log('You win! Paper beats Rock!');
-        console.log('You:' + ++playerScore);
-        console.log('Computer:' + computerScore);
+        return win;
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
         alert('You win! Scissors beats Paper!');
         console.log('You win! Scissors beats Paper!');
-        console.log('You:' + ++playerScore);
-        console.log('Computer:' + computerScore);
+        return win;
     }
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
         alert('You win! Rock beats Scissors');
         console.log('You win! Rock beats Scissors!');
-        console.log('You:' + ++playerScore);
-        console.log('Computer:' + computerScore);
+        return win;
     }
     else if (playerSelection.toLowerCase() == computerSelection) {
         alert('Tie!');
         console.log('Tie!');
-        console.log('You:' + playerScore);
-        console.log('Computer:' + computerScore);
+        return tie;
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
         alert('You lose! Rock beats Scissors.');
         console.log('You lose!');
-        console.log('You:' + playerScore);
-        console.log('Computer:' + ++computerScore);
+        return lose;
     }
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
         alert('You lose! Paper beats Rock.');
         console.log('You lose!');
-        console.log('You:' + playerScore);
-        console.log('Computer:' + ++computerScore);
+        return lose;
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
         alert('You lose! Scissors beats Paper.');
         console.log('You lose!');
-        console.log('You:' + playerScore);
-        console.log('Computer:' + ++computerScore);
+        return lose;
     }
     else {
         null
@@ -68,27 +61,46 @@ function playRound(playerSelection, computerSelection) {
 let playerSelection = 'rock'
 let playerScore = 0;
 let computerScore = 0;
+let win = 'win'; 
+let lose = 'lose'; 
+let tie = 'tie';
 
-function Game() {
-    if (playerScore > computerScore) {
-        alert(`Congratulations! You beat the computer with a score of ${playerScore}:${computerScore}!`)
-        console.log('Congratulations! You beat the computer!')
+function game(playRound) {
+    if (playRound == win) {
+        console.log('You:' + ++playerScore);
+        console.log('Computer:' + computerScore);
     }
-    else if (playerScore < computerScore) {
-        alert(`You lost against the computer with a score of ${playerScore}:${computerScore}!`)
-        console.log('You lost against the computer!')
-    }   
-    else if (playerScore == computerScore) {
-        alert(`Game is tied with a score of ${playerScore}:${computerScore}!`)
-        console.log('Game is a tie!')
+    else if (playRound == lose) {
+        console.log('You:' + playerScore);
+        console.log('Computer:' + ++computerScore);
     }
-    else {
-        null;
+    else if (playRound == tie) {
+        console.log('You:' + playerScore);
+        console.log('Computer:' + computerScore);
     }
 }
 
-playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice()); 
-playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice()); 
-playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice());  
-playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice());  
-Game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+function results() {
+    if (playerScore > computerScore) {
+        alert(`Congratulations! You beat the computer with a score of ${playerScore}:${computerScore}!`)
+        console.log(`Congratulations! You beat the computer with a score of ${playerScore}:${computerScore}!`)
+    }
+    else if (playerScore < computerScore) {
+        alert(`You lost against the computer with a score of ${playerScore}:${computerScore}!`)
+        console.log(`You lost against the computer with a score of ${playerScore}:${computerScore}!`)
+    }   
+    else if (playerScore == computerScore) {
+        alert(`Game is tied with a score of ${playerScore}:${computerScore}!`)
+        console.log(`Game is a draw with a score of ${playerScore}:${computerScore}!`)
+    }
+    else {
+        null;
+    }   
+}
+
+game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+game(playRound(prompt('Rock, Paper, or Scissors?'), getComputerChoice())); 
+results();
